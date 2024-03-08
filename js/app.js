030fe390aeb15370108a6f82c8097ea99d159243
Francesca-Barletta <wallebar@gmail.com>
2024-03-08 15:35:51 +0100

    add images in html from javascript

---
+++ js/app.js
@@ -0,0 +1,45 @@
+/*Consegna:
+Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+MILESTONE 1
+Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine 
+grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+MILESTONE 2
+Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente 
+servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
+Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1,
+ma costruito dinamicamente attraverso JavaScript.
+MILESTONE 3
+Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva,
+che quindi verrà visualizzata al posto della precedente.
+BONUS 1:
+Aggiungere il ciclo infinito del carosello. Ovvero se è attiva la prima immagine e l'utente
+clicca la freccia per andare all’immagine precedente, dovrà comparire l’ultima immagine dell’array e viceversa.
+BONUS 2:
+Aggiungere la visualizzazione di tutte le thumbnails sulla destra dell’immagine grande attiva,
+come nello screenshot proposto. Tutte le miniature avranno un layer di opacità scura,
+tranne quella corrispondente all’immagine attiva, che invece avrà un bordo colorato.
+Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.*/
+
+
+//commento la parte relativa alle immagini nell'html
+//mi copio le path delle immagini in una array
+
+const imgArray = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"];
+//console.log(imgArray);
+//devo accedere a tutti gli elementi dell'array
+//creo un ciclo for per leggere tutta la array
+// fuori dal ciclo for mi prendo l'elemento del dom dove inserire l'html
+const imgItemDOMElment = document.querySelector('.items-container');
+
+
+
+
+for(let i = 0; i < imgArray.length; i++){
+   //console.log(imgArray[i]) 
+   // con i ciclo for inserisco nell'html le immagini
+ imgItemDOMElment.innerHTML += `
+    <div class="item">
+        <img src="${imgArray[i]}" alt="">
+    </div>`
+}
